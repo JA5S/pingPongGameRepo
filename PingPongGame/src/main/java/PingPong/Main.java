22bceb18446d66222f99714520a218d5ce6ad816
JA5S <shumanja5@gmail.com>
2022-06-24 15:34:40 -0400

    Updated documentation

--- PingPongGame/src/main/java/PingPong/Main.java
+++ PingPongGame/src/main/java/PingPong/Main.java
@@ -1,7 +1,9 @@
 /**
  * File: Main.java
  * Group: Project Team 2
- * Author: Jonah Shuman Student
+ * Authors: Jonah Shuman,
+ * Zeleke Werssa, and
+ * Asha Azariah-Kribbs
  * Project: PingPongGame
  * Date: June 18, 2022
  * Purpose: Main program
