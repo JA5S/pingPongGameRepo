d473079dc6dbf3678fa44c7c4af5cfa36c716e99
JA5S <shumanja5@gmail.com>
2022-06-24 15:30:43 -0400

    Added resume and returnToTitle button functionality

--- PingPongGame/src/main/java/PingPong/PauseScreen.java
+++ PingPongGame/src/main/java/PingPong/PauseScreen.java
@@ -10,21 +10,49 @@
 package PingPong;
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class PauseScreen extends JPanel{
     
-    private final JLabel pauseLbl = new JLabel("Game Paused");
-    private final JButton pauseResume = new JButton("pauseResume Game");
-    private final JButton returnToTitle = new JButton("Return To Title");
+    private final JLabel pauseLabel = new JLabel("Game Paused");
+    private final JButton resumeBtn = new JButton("Resume Game");
+    private final JButton returnToTitleBtn = new JButton("Return To Title");
     private final Main main;
     
     PauseScreen(Main main)
     {
         this.main = main;
-        pauseLbl.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
-        this.add(pauseLbl);
-        this.add(pauseResume);
-        this.add(returnToTitle);
+        pauseLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
+        this.add(pauseLabel);
+        
+        this.add(resumeBtn);
+        resumeBtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                resumeGame();
+            }
+        });
+        
+        this.add(returnToTitleBtn);
+        returnToTitleBtn.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                returnToTitle();
+            }
+        });
+        
+    }
+    
+    private void resumeGame()
+    {
+        main.switchScreen(ScreenEnum.GAME);
+    }
+    
+    private void returnToTitle()
+    {
+        main.switchScreen(ScreenEnum.TITLE);
     }
 }
