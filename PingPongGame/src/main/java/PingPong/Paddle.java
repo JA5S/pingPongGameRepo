40581302575ecb037b39befd8a19776778b6d610
JA5S <shumanja5@gmail.com>
2022-06-30 15:55:05 -0400

    Added getter/setter methods for paddle speed/direction

--- PingPongGame/src/main/java/PingPong/Paddle.java
+++ PingPongGame/src/main/java/PingPong/Paddle.java
@@ -17,6 +17,7 @@ public class Paddle extends Rectangle{
 
     private int left, right, top, bottom;
     private int speed = 5;
+    private int direction = 1;
     
     public Paddle(int x, int y, int width, int height) 
     {
@@ -32,8 +33,25 @@ public class Paddle extends Rectangle{
         g2.fillRect(x, y, width, height);
     }
     
+    //Getter methods
     public int getSpeed()
     {
         return speed;
+    }
+    
+    public int getDirection()
+    {
+        return direction;
+    }
+    
+    //Setter methods
+    public void setDirection(int dir)
+    {
+        direction = dir;
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
     }
 }
